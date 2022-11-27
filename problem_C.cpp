aaa9f134b74b5841370efa5fc3dd78e90ac2ccbe
holyguacamoleCoder <2226192528@qq.com>
2022-11-27 19:22:22 +0800

    QQ等级

---
+++ problem_C.cpp
@@ -0,0 +1,32 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//void rank(int n)
+//{
+//	if (n < 4 && n>=1)
+//	{
+//		printf("*");
+//		rank(n - 1);
+//	}
+//	else if (n < 16 && n >= 4)
+//	{
+//		printf("(");
+//		rank(n - 4);
+//	}
+//	else if (n >= 16 && n < 64)
+//	{
+//		printf("O");
+//		rank(n - 16);
+//	}
+//	else if(n>=64)
+//	{
+//		printf("W");
+//		rank(n-64);
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	rank(n);
+//	return 0;
+//}
